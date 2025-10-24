e3ef9b42e88420c845b149d1134847b216d05f70
Tim Black <timblaktu@gmail.com>
2025-10-23 19:06:03 -0700

    Update test files for container build

--- tests/unit/ini_parser_test.cpp
+++ tests/unit/ini_parser_test.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <codecvt>
 #include <locale>
+#include <iostream>
 
 // We need to include the structures and function from plugin.cpp
 // Since we can't include plugin.cpp directly (it has DLL entry points),
@@ -201,6 +202,13 @@ TEST_F(IniParserTest, EdgeCases) {
     
     // Should handle whitespace and comments properly
     // Note: incomplete_section has no closing bracket so should be ignored
+    
+    // Debug: Let's see what we actually got
+    std::wcout << L"Found " << result.bareDisks.size() << L" bare disks:" << std::endl;
+    for (size_t i = 0; i < result.bareDisks.size(); ++i) {
+        std::wcout << L"  [" << i << L"] uuid=" << result.bareDisks[i].uuid << L", label=" << result.bareDisks[i].label << std::endl;
+    }
+    
     EXPECT_EQ(result.bareDisks.size(), 2); // bare_disk_1 and bare_disk_unicode  
     EXPECT_EQ(result.vhdxs.size(), 1); // vhdx_1
     
